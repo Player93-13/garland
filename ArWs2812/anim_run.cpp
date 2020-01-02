304ac1120ab58658ee45e03614bd405ab54022b6
Player93-13 <57127746+Player93-13@users.noreply.github.com>
2020-01-02 21:55:23 +0300

    bug fix

--- ArWs2812/anim_run.cpp
+++ ArWs2812/anim_run.cpp
@@ -3,26 +3,25 @@
 #include "palette.h"
 
 void Anim::animRun_SetUp() {
-    pos = 0;
-    inc = 1 + (rngb() >> 5);
-    if (random(10) > 5) {
-        inc = -inc;
-    }
+  pos = 0;
+  inc = 1 + (rngb() >> 5);
+  if (random(10) > 5)
+    inc = -inc;
 }
 
 void Anim::animRun_Run() {
-    
-    int p = pos;
-    for (int i=0;i<LEDS;i++) {
-        Color c = palette->getPalColor((float)p/256);
-        leds[i] = c;
-   
-        p = p + inc;
-        if (p >= 256) {
-            p = p - 256;
-        } else if (p < 0) {
-            p = p + 256;
-        }
-    }
-    pos = pos + 1;
+
+  int p = pos;
+  for (int i = 0; i < LEDS; i++)
+  {
+    Color c = palette->getPalColor((float)p / 256);
+    leds[i] = c;
+
+    p += inc;
+    if (p >= 256)
+      p = p - 256;
+    else if (p < 0)
+      p = p + 256;
+  }
+  pos = pos + 1;
 }
