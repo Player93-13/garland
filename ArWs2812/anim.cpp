e10e5719af0264000f77df13c36fb89981bef9f1
Player93-13 <57127746+Player93-13@users.noreply.github.com>
2020-01-04 16:52:12 +0300

    Revert "Revert "bug fix""
    This reverts commit 8379653c049b0554f75407a98bb79464110dc5ac.

--- ArWs2812/anim.cpp
+++ ArWs2812/anim.cpp
@@ -44,15 +44,16 @@ bool Anim::run()
   float transc = (float)((long)transms - (long)millis()) / TRANSITION_MS;
   Color * leds_prev = (leds == leds1) ? leds2 : leds1;
 
-  for (int i = 0; i < LEDS; i++) {
-    Color c = leds[i].clone();
+  for (int i = 0; i < LEDS; i++)
+  {
+    Color c = leds[i];
     
     if (transc > 0)  //transition is in progress
-      Color c = c.interpolate(leds_prev[i], transc);
-      
+      c = c.interpolate(leds_prev[i], transc);     
+
     c.setbrightness(i < GARL ? BRIGHTNESS : STARBRIGHTNESS);
     c.gammaCorrection();
-    
+
     if (i < GARL)
       pixels.setPixelColor(i, pixels.Color(c.r, c.g, c.b));
     else
