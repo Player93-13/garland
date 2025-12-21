b50ac0acc79c29f3fd99ba10575a7ecf0243eae6
Player93-13 <57127746+Player93-13@users.noreply.github.com>
2025-12-21 12:46:13 +0300

    fix: Переполнение цветов в animSpread

--- espAll/src/anim_spread.cpp
+++ espAll/src/anim_spread.cpp
@@ -7,20 +7,26 @@
 void Anim::animSpread_SetUp() {
     inc = random(2,8);
     memset(seq, 0, LEDS_ALL);
+    memset(ledstmp, 0, sizeof(Color) * LEDS_ALL);
 }
 
 void Anim::animSpread_Run() {
-    memset(leds, 0, 3*LEDS_ALL);
+    memset(leds, 0, sizeof(Color) * LEDS_ALL);
 
     for (int i=0;i<LEDS_ALL;i++) {
         if (seq[i] > 0) {
             int width = SPREAD_MAX_WIDTH - seq[i];
-            for (int j=i-width;j<=(i+width);j++) {
+            int start = max(0, i - width);
+            int end = min(LEDS_ALL-1, i + width);
+            for (int j = start; j <= end; j++) {
                 Color c = ledstmp[i];
                 if (j>=0 && j<LEDS_ALL) {
-                    leds[j].r += c.r;
-                    leds[j].g += c.g;
-                    leds[j].b += c.b;
+                    uint16_t t = leds[j].r + c.r;
+                    leds[j].r = (t > 255) ? 255 : t;
+                    t = leds[j].g + c.g;
+                    leds[j].g = (t > 255) ? 255 : t;
+                    t = leds[j].b + c.b;
+                    leds[j].b = (t > 255) ? 255 : t;
                 }
             }
             ledstmp[i].fade(255/SPREAD_MAX_WIDTH);
